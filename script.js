1d7a31067cf716d5879927c857ed15617f721512
Mayank Goel <goelmayank2000@gmail.com>
2023-05-31 01:14:53 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const heading1 = React.createElement("h1", {}, "heading 1");
+const heading2 = React.createElement("h2", {}, "heading 2");
+
+const container = React.createElement("div", {}, [heading1, heading2]);
+root.render(container);
